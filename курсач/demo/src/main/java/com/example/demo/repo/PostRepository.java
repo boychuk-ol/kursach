ae4635db844d9d703725dec10a5b3a0dbc1db4f5
boychuk-ol <79440042+boychuk-ol@users.noreply.github.com>
2022-05-24 19:34:19 +0300

    Delete курсач/demo/src/main/java/com/example/demo/repo directory

--- курсач/demo/src/main/java/com/example/demo/repo/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repo;
-
-
-import com.example.demo.models.Post;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<Post, Long> {
-}
